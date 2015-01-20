98832df30c20c4deb5ba7943d0a95ac1234f1e47
Gocho Mugo <mugo@forfuture.co.ke>
2015-01-20 17:33:29 +0300

    Fix error and Unique fields in table members
    Unique username and emails are accepted in the table
    members.

--- utils.js
+++ utils.js
@@ -4,7 +4,7 @@
 // checks if a req has all params
 exports.checkParams = function(req, keysArray) {
   var idx = 0;
-  for (idx; idx keysArray.length; idx++) {
+  for (idx; idx < keysArray.length; idx++) {
     if (! req.param(keysArray[idx])) { return keysArray[idx]; }
   }
   return null;
@@ -12,5 +12,5 @@ exports.checkParams = function(req, keysArray) {
 
 
 exports.sqlStr = {
-  insertMember: "insert into members(username, email, password, email_updates) values(%s, %s, %s, true)"
+  insertMember: 'insert into members(username, email, password, email_updates) values("%s", "%s", "%s", true)'
 };
